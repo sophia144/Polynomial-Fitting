3aba6d17ba6d888e78242550fd6ddac4c6d7f592
Sophia Pierce <piercesp@outlook.com>
2025-11-13 15:25:08 +0000

    Calculated chi squared

--- cod.py
+++ cod.py
@@ -29,13 +29,17 @@ plt.xlabel(x_axis)
 plt.ylabel(f"{y_axis_label} {y_coefficient_word}")
 plt.title(title)
 
-#plotting the polynomial fits
-coefficients = []
-poly_function = []
+#polynomial fits
 for order in range(1, 10):
+    #calculations and plotting
     coefficients = np.polyfit(x_coords, y_coords, order)
     poly_function = np.poly1d(coefficients)
-    plt.plot(x_coords, poly_function(x_coords), '--')
+    plt.plot(x_coords, poly_function(x_coords), alpha=0.3)
+    
+    #working out chi squared
+    uncertainty = 1
+    residuals = y_coords - poly_function(x_coords)
+    chi_squared = np.sum((residuals/uncertainty)**2)
+
 
 plt.show()
-
