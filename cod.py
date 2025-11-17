360abcc80e5211a97a6beed7e49eb1fd6a1840c1
Sophia Pierce <piercesp@outlook.com>
2025-11-17 11:48:24 +0000

    Added a projection for the latest ten years of the dataset

--- cod.py
+++ cod.py
@@ -6,7 +6,10 @@ import numpy as np
 #cleaning the dataset
 full_df = pd.read_csv('long-term-cod-catch.csv')
 full_df.drop(columns=['Code'], inplace=True)
-recent_df = full_df[full_df['Year'] > 1920]
+start_100_period = 1920
+end_100_period = 2020
+df_100 = full_df[full_df['Year'] > start_100_period]
+df_90 = df_100[full_df['Year'] < end_100_period - 10]
 
 #setting up parameters for plotting
 x_axis = "Year"
@@ -20,26 +23,43 @@ y_coefficient_figure = 1000000
 title = "Polynomial Fits for Northern Atlantic Cod Catches in Eastern Canada"
 
 #writing coordinates to numpy arrays
-x_coords = recent_df[x_axis]
-y_coords = recent_df[y_axis]/y_coefficient_figure
+x_coords_100 = df_100[x_axis]
+y_coords_100 = df_100[y_axis]/y_coefficient_figure
+
+x_coords_90 = df_90[x_axis]
+y_coords_90 = df_90[y_axis]/y_coefficient_figure
 
 #plotting the main data
-plt.plot(x_coords, y_coords)
+plt.grid()
+plt.scatter(x_coords_100, y_coords_100, s=12)
 plt.xlabel(x_axis)
 plt.ylabel(f"{y_axis_label} {y_coefficient_word}")
-plt.title(title)
+plt.ylim(-0.2, 1)
+plt.title(title, pad=20)
+
+#plotting the ten year mark
+plt.axvline(x = end_100_period - 10, color = 'midnightblue', linestyle = 'dotted')
 
 #polynomial fits
+order_vals = []
+chi_squared_vals = []
 for order in range(1, 10):
     #calculations and plotting
-    coefficients = np.polyfit(x_coords, y_coords, order)
+    coefficients = np.polyfit(x_coords_90, y_coords_90, order)
     poly_function = np.poly1d(coefficients)
-    plt.plot(x_coords, poly_function(x_coords), alpha=0.3)
-    
-    #working out chi squared
-    uncertainty = 1
-    residuals = y_coords - poly_function(x_coords)
-    chi_squared = np.sum((residuals/uncertainty)**2)
+
+    #chi squared calculation
+    residuals = poly_function(x_coords_90) - y_coords_90
+    sum_squared_residuals = 0
+    for residual in residuals:
+        sum_squared_residuals += residual ** 2
+    order_vals.append(order)
+    chi_squared_vals.append(sum_squared_residuals/len(x_coords_90))
 
 
-plt.show()
+    plt.plot(x_coords_100, poly_function(x_coords_100), color='black', alpha=0.3, lw=1.8)
+
+print(order_vals)
+print(chi_squared_vals)
+
+#plt.show()
